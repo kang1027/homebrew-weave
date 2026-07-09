e8188e54d5f69fc1327afca762e62bc48d8d14eb
kang1027 <kang3171611@naver.com>
2026-07-09 15:25:58 +0900

    feat: weave-pt cask 0.1.18

--- Casks/weave-pt.rb
+++ Casks/weave-pt.rb
@@ -1,6 +1,6 @@
 cask "weave-pt" do
-  version "0.1.17"
-  sha256 "853265d0db7df045db83350b721f3830559302c15c4f98d107d86dc9170a4ee5"
+  version "0.1.18"
+  sha256 "35c491a08d9e93d23bc4412a25f583b68d4daa162c827f063a3f3675e8b09b63"
 
   url "https://github.com/kang1027/Weave/releases/download/v#{version}/Weave-#{version}.zip"
   name "Weave"
